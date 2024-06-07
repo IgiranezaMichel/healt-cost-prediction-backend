d10ecf5919fe1e7e837921b6372d28dbae3706d3
IgiranezaMichel <igiramichael@gmail.com>
2024-06-07 10:05:27 +0200

    add new spring project

---
+++ src/main/java/com/healt_cost_prediction/HealtCostPredictionApplication.java
@@ -0,0 +1,12 @@
+package com.healt_cost_prediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class HealtCostPredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealtCostPredictionApplication.class, args);
+	}
+
+}
