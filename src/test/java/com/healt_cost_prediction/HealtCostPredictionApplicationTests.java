89bdf7e67298bb9763c918f84326d94b9e0acade
IgiranezaMichel <igiramichael@gmail.com>
2024-06-12 20:33:20 +0200

    add helth cost prediction apploication test

---
+++ src/test/java/com/healt_cost_prediction/HealtCostPredictionApplicationTests.java
@@ -0,0 +1,22 @@
+package com.healt_cost_prediction;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.healt_cost_prediction.services.ProductDetailService;
+
+@SpringBootTest
+class HealtCostPredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	@Autowired ProductDetailService p=new ProductDetailService();
+@Test
+void save(){
+ 
+}
+@Test
+void predict(){
+	 
+}
+}
