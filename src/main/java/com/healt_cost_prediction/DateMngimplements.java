53796a1426c05b5b5fe5e775a270d85d2a6f987a
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-07-16 10:58:25 +0200

    create interface

---
+++ src/main/java/com/healt_cost_prediction/DateMngimplements.java
@@ -0,0 +1,14 @@
+package com.healt_cost_prediction;
+@FunctionalInterface
+interface MyException{
+    int number(int number);
+}
+public class DateMngimplements {
+public static void main(String arg[]){
+   MyException ex=number ->(int) Math.pow(number, 2);
+    int number=ex.number(10);
+   System.out.println(number);
+}
+
+ 
+}
